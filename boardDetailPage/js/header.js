edcd152651ac0f289407af8501645e76902f07a4
HEEWON0010 <chw001025@gmail.com>
2025-12-14 15:41:50 +0900

    added boardDetailPage
    글 상세 내용 페이지 구현 | json 테스트 데이터 연동

---
+++ boardDetailPage/js/header.js
@@ -0,0 +1,15 @@
+// 헤더 
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector("header");
+    let lastScrollY = 0;
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > lastScrollY) {
+            header.classList.add("hide");
+        } else {
+            header.classList.remove("hide");
+        }
+        lastScrollY = window.scrollY;
+    });
+});
+
